d785562f243e46960c74e8732d72daa8d95aeaab
Taan <tannguyen1220@gmail.com>
2022-08-05 18:25:19 +0700

    refactor nav bar

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,21 +1,31 @@
 import { buildTimeValue } from '@testing-library/user-event/dist/utils';
 import React from 'react';
-import { Row, Col, Navbar, Nav, Container } from 'react-bootstrap';
+import { Row, Col, Navbar, Nav, Container, Form, Button } from 'react-bootstrap';
 import { FaReact } from 'react-icons/fa';
+import { GoSearch } from 'react-icons/go';
 import './layout.css';
 
 
 
 const Layout = () => {
     return (
-        <Navbar bg='dark' variant='dark' >
+        <Navbar variant='dark' className="header" >
             <Container>
                 <Navbar.Brand href="#home">React-1  <FaReact /></Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav.Link href="#features">Boring Stuff</Nav.Link>
+                    <Nav.Link href="#pricing">News</Nav.Link>
                 </Nav>
+                <Form className="d-flex">
+                    <Form.Control
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                    />
+                    <Button variant="dark"><GoSearch /></Button>
+                </Form>
             </Container>
         </Navbar>
     )
